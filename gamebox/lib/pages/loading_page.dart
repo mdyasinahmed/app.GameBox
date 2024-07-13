9bb024973054042f7a1aedab803192f4bcaf2e9b
mdyasinahmed <yasinahmed7875@gmail.com>
2024-07-13 23:53:19 +0600

    add app loading page

---
+++ gamebox/lib/pages/loading_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+// import 'home_page.dart'; // Import the home page
+
+class LoadingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Spacer(flex: 2),
+            Align(
+              alignment: Alignment.center,
+              child: SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset('assets/logo.png'),
+              ),
+            ),
+            Spacer(flex: 3),
+            ElevatedButton(
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => HomePage()),
+                // );
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 12.0),
+                textStyle: TextStyle(fontSize: 16),
+                shadowColor: Colors.transparent,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFFA11818), Color(0xFFFF5F5F)],
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 12.0),
+                child: Text(
+                    "Let's Play"
+                      style: TextStyle(color: Color(0xFF474747), fontSize: 12),
+                ),
+              ),
+            ),
+            Spacer(flex: 1),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Column(
+                children: [
+                  Text(
+                    'POWERED BY',
+                    style: TextStyle(color: Color(0xFF474747), fontWeight: FontWeight.bold, fontSize: 12),
+                  ),
+                  SizedBox(height: 5),  // Reduced the height here
+                  SizedBox(
+                    width: 100,
+                    height: 80,
+                    child: Image.asset('assets/organisation_logo.png'),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '© 2024 GameBox',
+                    style: TextStyle(color: Color(0xFF474747), fontSize: 12),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
